00984dc5037ecfb863ad088244d86a4688c489c6
Juan Camilo Victoria <jcvictoriap@gmail.com>
2025-07-11 11:29:49 -0500

    Initial Commit

---
+++ TechHackNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TechHackNews
+//
+//  Created by Juan Camilo Victoria Pacheco on 11/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TechHackNews/TechHackNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechHackNewsApp.swift
+//  TechHackNews
+//
+//  Created by Juan Camilo Victoria Pacheco on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TechHackNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
